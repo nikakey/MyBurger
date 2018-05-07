0d69ea476ab17e90c2f4f9edeec7fbe5fa79dc6a
Veronika Kochetkova <veronica90k@gmail.com>
2018-05-07 15:12:04 -0700

    Add a model for an order summary

---
+++ src/components/Burger/OrderSummary/OrderSummory.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Aux from '../../../hoc/Aux';
+
+const orderSummory = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients).map(ingKey => {
+    return <li key={ingKey}><span style={{textTransform: 'capitalize'}}>{ingKey}:</span> {props.ingredients[ingKey]}</li>
+  });
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p>Contunue to Checkout?</p>
+    </Aux>
+  );
+};
+
+export default orderSummory;
