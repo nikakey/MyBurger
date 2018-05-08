fa0f17177ca3af939bcda6ca8e924f6543adf6e2
Veronika Kochetkova <veronica90k@gmail.com>
2018-05-08 16:11:52 -0700

    Change Menu to menu icon for the mobile screen size

--- src/components/Navigation/SideDrawer/Toggle/Toggle.js
+++ src/components/Navigation/SideDrawer/Toggle/Toggle.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import classes from './Toggle.css';
 
 const toggle = (props) => (
-  <div onClick={props.clicked}>Menu</div>
+  <div 
+    onClick={props.clicked}
+    className={classes.DrawerToggle}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
 );
 
 export default toggle;
