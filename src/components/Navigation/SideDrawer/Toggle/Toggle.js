22c92749e69de00ddd78ab4937805d6ab3260ff0
Veronika Kochetkova <veronica90k@gmail.com>
2018-05-08 16:07:04 -0700

    Set sideDrawer toggle btn to work

---
+++ src/components/Navigation/SideDrawer/Toggle/Toggle.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const toggle = (props) => (
+  <div onClick={props.clicked}>Menu</div>
+);
+
+export default toggle;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Toggle from '../SideDrawer/Toggle/Toggle';
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>Menu</div>
+    <Toggle clicked={props.toggleClicked}/>
     <div className={classes.Logo}>
       <Logo />
     </div>
