8592c4d67fd7f723e4eb6327fdbc8f989ca2fc68
Veronika Kochetkova <veronica90k@gmail.com>
2018-05-08 09:39:52 -0700

    Implement custom button component: set two buttons (Cansel and Continue) to work

--- src/components/Burger/OrderSummary/OrderSummory.js
+++ src/components/Burger/OrderSummary/OrderSummory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummory = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map(ingKey => {
@@ -13,8 +14,8 @@ const orderSummory = (props) => {
         {ingredientSummary}
       </ul>
       <p>Contunue to Checkout?</p>
-      <button>Cansel</button>
-      <button>Continue</button>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>Cansel</Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
     </Aux>
   );
 };
