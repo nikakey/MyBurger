424907fd4f4ef637d70761bba5674e2e91a41348
Veronika Kochetkova <veronica90k@gmail.com>
2018-05-25 14:02:28 -0700

    Set logic to submit an order to the db

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,34 +90,12 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!');
-    // this.setState({loading: true});
-
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice, //todo: calculate this price on the server in the prodaction (real app) to not allow a user to manipulate this price
-    //   customer: {
-    //     name: 'Nika Nika',
-    //     address: 'Teststreet 1',
-    //     zipCode: '12345',
-    //     country: 'Hanalulu',
-    //     email: 'test@test.com'
-    //   },
-    //   deliveryMethod: 'fastest'
-    // }
-
-    // axios.post('/orders.json', order)
-    //   .then(response => {
-    //     this.setState({loading: false, purchasing: false});
-    //   })
-    //   .catch(error => {
-    //     this.setState({loading: false, purchasing: false});
-    //   });
     const queryParams = [];
     for (let i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
+    queryParams.push('price=' + this.state.totalPrice);
     const queryString = queryParams.join('&');
-
 
     this.props.history.push({
       pathname: '/checkout',
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
   
@@ -10,7 +11,33 @@ class ContactData extends Component {
     address: {
       street: '',
       postalCode: ''
+    },
+    loading: false
+  }
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    this.setState({loading: true});
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price, //todo: calculate this price on the server in the prodaction (real app) to not allow a user to manipulate this price
+      customer: {
+        name: 'Nika Nika',
+        address: 'Teststreet 1',
+        zipCode: '12345',
+        country: 'Hanalulu',
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
     }
+
+    axios.post('/orders.json', order)
+      .then(response => {
+        this.setState({loading: false});
+      })
+      .catch(error => {
+        this.setState({loading: false});
+      });
   }
 
   render () {
@@ -22,7 +49,7 @@ class ContactData extends Component {
           <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
           <input className={classes.Input} type="text" name="street" placeholder="Street" />
           <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-          <Button btnType="Success">Order</Button>
+          <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
         </form>
       </div>
     );
