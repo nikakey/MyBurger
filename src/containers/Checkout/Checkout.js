5fb6f0aa8269d47788dcd8dbe2aaf7918a18a87e
Veronika Kochetkova <veronica90k@gmail.com>
2018-05-31 12:00:04 -0700

    Redirecting after purchase

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,8 +4,13 @@ import {connect} from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
+import * as orderActions from '../../store/actions/index';
 
 class Checkout extends Component {
+
+  componentWillMount () {
+    this.props.onInitPurchase();
+  }
 
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
@@ -18,8 +23,10 @@ class Checkout extends Component {
   render () {
     let summary = <Redirect to="/" />
     if(this.props.ings) {
+      const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
       summary = (
         <div>
+          {purchasedRedirect}
           <CheckoutSummary 
             ingredients={this.props.ings}
             checkoutCancelled={this.checkoutCancelledHandler}
@@ -36,8 +43,15 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.burgerBuilder.ingredients
+    ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased
   };
 };
 
-export default connect(mapStateToProps)(Checkout);
+const mapDispatchToProps = dispatch => {
+  return {
+    onInitPurchase: () => dispatch(orderActions.purchaseInit())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
