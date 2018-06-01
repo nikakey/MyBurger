015a95d234f5045d7d55d839f0602647c517dd57
Veronika Kochetkova <veronica90k@gmail.com>
2018-06-01 13:11:42 -0700

    Add sign-up/sign-in switching

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -40,7 +40,8 @@ class Auth extends Component {
         valid: false,
         touched: false
       }
-    }
+    },
+    isSignUp: true
   }
 
   validityCheck (value, rules) {
@@ -87,7 +88,13 @@ class Auth extends Component {
 
   submitHandler = (event) => {
     event.preventDefault();
-    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignUp);
+  }
+
+  switchAuthModeHandler = () => {
+    this.setState(prevState => {
+      return {isSignUp: !prevState.isSignUp};
+    });
   }
   
   render () {
@@ -120,6 +127,11 @@ class Auth extends Component {
           {form}
           <Button btnType="Success">Submit</Button>
         </form>
+        <Button 
+          btnType="Danger"
+          clicked={this.switchAuthModeHandler}>
+          Switch To {this.state.isSignUp ? 'Signin' : 'Signup'}
+        </Button>
       </div>
     );
   }
@@ -127,7 +139,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth: (email, password) => dispatch(actions.auth(email, password))
+    onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
   };
 };
 
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignUp) => {
   return dispatch => {
     dispatch(authStart());
     const authData = {
@@ -30,7 +30,12 @@ export const auth = (email, password) => {
       password: password,
       returnSecureToken: true
     };
-    axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAoq8_lcm_4Tc70rJF5mj92OFPSGsVzs1g', authData)
+    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAoq8_lcm_4Tc70rJF5mj92OFPSGsVzs1g';
+    if(!isSignUp) {
+      url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAoq8_lcm_4Tc70rJF5mj92OFPSGsVzs1g';
+    }
+
+    axios.post(url, authData)
       .then(response => {
         console.log(response);
         dispatch(authSuccess(response.data));
