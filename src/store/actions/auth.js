4b12b50ebd240b8ec2e88a43e3df1c75ce732bbb
Veronika Kochetkova <veronica90k@gmail.com>
2018-06-01 12:56:25 -0700

    Add sign-in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -22,7 +24,20 @@ export const authFail = (error) => {
 
 export const auth = (email, password) => {
   return dispatch => {
-    //...
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAoq8_lcm_4Tc70rJF5mj92OFPSGsVzs1g', authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
   };
 };
